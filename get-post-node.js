06e72aefa8659df97dfdfc6021f59571b4978c1b
Bryan Serif <bserif2103@gmail.com>
2015-11-15 21:07:32 -0500

    updated start address

--- get-post-node.js
+++ get-post-node.js
@@ -49,5 +49,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://52.26.37.94:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
